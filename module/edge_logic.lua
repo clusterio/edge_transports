ddaa63cf35fa3b3860555e7fdfb7b4eba8ffac85
Daniel <danielv@danielv.no>
2023-01-01 21:57:40 +0100

    Check entity validity before accessing in event handlers (#3)

--- module/edge_logic.lua
+++ module/edge_logic.lua
@@ -253,7 +253,7 @@ local function create_belt_link(id, edge, offset, entity)
 end
 
 local function on_built(entity)
-	if is_transport_belt[entity.name] then
+	if entity.valid and is_transport_belt[entity.name] then
 		local pos = {entity.position.x, entity.position.y}
 		for id, edge in pairs(global.edge_transports.edges) do
 			if edge.active and game.surfaces[edge.surface] == entity.surface then
@@ -279,7 +279,7 @@ local function remove_belt_link(id, edge, offset, entity)
 end
 
 local function on_removed(entity)
-	if is_transport_belt[entity.name] then
+	if entity.valid and is_transport_belt[entity.name] then
 		local pos = {entity.position.x, entity.position.y}
 		for id, edge in pairs(global.edge_transports.edges) do
 			if edge.active and game.surfaces[edge.surface] == entity.surface then
